20c79af9b8937e5bbee92d5f747281accdd04e78
Svetlana Popova <pss96.svetlana@gmail.com>
2023-07-09 00:37:35 +0300

    Refactor test practice form

--- tests/test_practice_form.py
+++ tests/test_practice_form.py
@@ -6,21 +6,18 @@ def test_student_registration_form():
     registration_page = RegistrationPage()
     registration_page.open()
 
-    (
-    registration_page
-        .fill_first_name('StudentName')
-        .fill_last_name('StudentName2')
-        .fill_user_email('name@ex.com')
-        .fill_gender('Female')
-        .fill_user_number('1234567890')
-        .fill_date_of_birth('1996', '12', '12')
-        .fill_subject('Physics')
-        .fill_hobby('Sports')
-        .fill_picture('icon.jpg')
-        .fill_address('438 DARK SPURT SAN FRANCISCO CA 94528 USA')
-        .fill_state('NCR')
-        .fill_city('Delhi')
-    )
+    registration_page.fill_first_name('StudentName')
+    registration_page.fill_last_name('StudentName2')
+    registration_page.fill_user_email('name@ex.com')
+    registration_page.fill_gender('Female')
+    registration_page.fill_user_number('1234567890')
+    registration_page.fill_date_of_birth('1996', '12', '12')
+    registration_page.fill_subject('Physics')
+    registration_page.fill_hobby('Sports')
+    registration_page.fill_picture('icon.jpg')
+    registration_page.fill_address('438 DARK SPURT SAN FRANCISCO CA 94528 USA')
+    registration_page.fill_state('NCR')
+    registration_page.fill_city('Delhi')
 
     registration_page.submit_form()
 
